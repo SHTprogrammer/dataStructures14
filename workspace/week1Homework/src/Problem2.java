dd3d9a1ec521fa490d0d2087ff3b66dcc682db2a
monicaBee <monica@gwynniebee.com>
2014-07-15 13:52:04 -0400

    left edge case added

--- workspace/week1Homework/src/Problem2.java
+++ workspace/week1Homework/src/Problem2.java
@@ -11,13 +11,14 @@ class Problem2
 	public static void main( String[] args)
 	{
 		int[] array1 = {1,2,3,4,5};
+		int k0 = 0;
 		int k1 = 2;
 		int k2 = 5;
 		int k3 = 6;
 		int[] array2 = {2,4,6,8};
 		int[] array3 = {0};
 		
-		
+		System.out.println(RecursiveBs.search(array1, k0));
 		System.out.println(RecursiveBs.search(array1, k1));
 		System.out.println(RecursiveBs.search(array1,k3));
 		System.out.println(RecursiveBs.search(array2,k2));

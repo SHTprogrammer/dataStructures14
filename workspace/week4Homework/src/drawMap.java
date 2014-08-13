82d594ebd16af7f2e81b3b69884690e301c23c91
quaintm <mquaintance@gmail.com>
2014-08-13 13:33:51 -0400

    added comments and corrent mapping of tree

--- workspace/week4Homework/src/drawMap.java
+++ workspace/week4Homework/src/drawMap.java
@@ -1,14 +1,30 @@
+/*
+ * drawMap contains methods:
+ * Constructor -- requires start and end city names as strings and LinkedLists
+ *   of edges and coordinates
+ *   
+ * paintComponent -- draws the solution path as a string, maps the cities at their 
+ *   coordinates, and draws the solution path 
+ */
+
+
 import javax.swing.*;
-
 import java.awt.*;
-import java.io.FileNotFoundException;
 import java.util.LinkedList;
+import java.util.Map;
 
+@SuppressWarnings("serial")
 public class drawMap extends JComponent{
-dijkstrasMap cityMap;
-public LinkedList<String> cityPairs;
-public LinkedList<String> cityLoc;
+	
+	dijkstrasMap cityMap;
+	public LinkedList<String> cityPairs;
+	public LinkedList<String> cityLoc;
+	
 
+    /*
+     * Constructor which takes in two city names as strings and two 
+     * LinkedLists of edges and coordinates
+     */
 	public drawMap(String startCity, String endCity, 
 			LinkedList<String> cityPairs, LinkedList<String> cityLoc){
 		setPreferredSize(new Dimension(900,400));
@@ -16,31 +32,36 @@ public LinkedList<String> cityLoc;
 		cityMap.findPath(startCity, endCity);
 	}
 
+	/*
+	 * Draws the solution path as a string, maps the cities at their 
+	 * coordinates, and draws the solution path
+	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+	 */
 	public void paintComponent(Graphics g){
 			Graphics g2 = (Graphics2D) g;
-			String pathInfo = (cityMap.pathNames + ": "+String.format("%.1g%n", cityMap.pathLength)+" miles");
+			
+//			draw solution text
+			String pathInfo = cityMap.StringPath;
 			g2.drawString(pathInfo, 250,10);
 			
-			for(int n = 0; n < cityMap.numNodes; ++n){
-				
-				String state = cityMap.StringPath;
-				
-				Integer x = ( 50+ ( 1+graph.cityMap.get(state).x)/4);
-				Integer y = (350-(1+graph.cityMap.get(state).y)/4);
-				g2.drawString(state,x,y);
-				
+//			map all cities
+			for(Map.Entry<String,Vertex> entry : cityMap.theCities.entrySet()) {
+				String cityName = entry.getKey();
+				Point coord = entry.getValue().coord;
+								
+				Integer x = ( 50+ ( 1+coord.x)/4);
+				Integer y = (350-(1+coord.y)/4);
+				g2.drawString(cityName,x,y);	
 			}
 
-			for(int n = 1; n < graph.shortestXY.size(); ++n){
-				Integer x2 = (50+(1+graph.shortestXY.get(n).x)/4);
-				Integer y2 = (350-(1+graph.shortestXY.get(n).y)/4);
-				Integer x1 = (50+(1+graph.shortestXY.get(n-1).x)/4);
-				Integer y1 = (350 - (1+graph.shortestXY.get(n-1).y)/4);
+//			draw solution path
+			for(int n = 1; n < cityMap.pathPoints.size(); ++n){
+				Integer x2 = (50+(1+cityMap.pathPoints.get(n).x)/4);
+				Integer y2 = (350-(1+cityMap.pathPoints.get(n).y)/4);
+				Integer x1 = (50+(1+cityMap.pathPoints.get(n-1).x)/4);
+				Integer y1 = (350 - (1+cityMap.pathPoints.get(n-1).y)/4);
 				g2.drawLine(x1,y1,x2,y2);
-			
-			}
-
-
 			}
 
 	}
+}

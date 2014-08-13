a99bf177651a0b3321d85dd5131e99fb177c0376
quaintm <mquaintance@gmail.com>
2014-08-13 13:34:09 -0400

    YOLO

--- workspace/week4Homework/src/Point.java
+++ workspace/week4Homework/src/Point.java
@@ -1,9 +1,14 @@
 public class Point{
 	Integer x;
 	Integer y;
-
+	
 	public Point(Integer x,Integer y){
 		this.x = x;
 		this.y = y;		
 	}
+	
+	public String toString(){
+		return ("("+x.toString()+","+y.toString()+")");
+	}
+	    
 }

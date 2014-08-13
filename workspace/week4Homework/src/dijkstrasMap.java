14548940983c3354fbbfbf6a79a763ad32de98ff
quaintm <mquaintance@gmail.com>
2014-08-13 13:07:40 -0400

    added coords to vertices, plus debugger

--- workspace/week4Homework/src/dijkstrasMap.java
+++ workspace/week4Homework/src/dijkstrasMap.java
@@ -1,3 +1,14 @@
+/*djikstrasMap contains methods:
+ * 
+ * Constructor -- required LinkedLists of edges, coordinates
+ * (assumes edges are bi-directional and there are no duplicate coordinates)
+ * 
+ * 
+ * 
+ */
+
+
+
 
 import java.text.DecimalFormat;
 import java.util.HashMap;
@@ -11,12 +22,14 @@ public class dijkstrasMap{
 	Double pathLength;
 	String pathNames;
 	String StringPath;
+	LinkedList<Point> pathPoints;
 	
 	int numNodes;
 	LinkedList<String> edges;
 	LinkedList<String> coords;
 	TreeMap<String,Vertex> theCities = new TreeMap<>();
 	HashMap<String,Vertex> cityHash = new HashMap<>();
+	HashMap<String,Point> coordHash = new HashMap<>();
 
 	/*
 	 * Constructor with edges and coordinate inputs
@@ -25,6 +38,32 @@ public class dijkstrasMap{
 		edges = e;
 		coords = c;
 
+//    	read city coordinates into hash map
+    	for ( int i =0; i < coords.size(); i++ ){
+    		String newLine = coords.get(i);
+    		StringTokenizer st = new StringTokenizer(newLine);
+    		String newCity = new String();
+    		String cityX = new String();
+    		String cityY = new String();
+			int itemCount = 0;
+
+			while (st.hasMoreTokens()) {		
+ 	    	     String nextItem = st.nextToken();	    	     
+ 	    	     if( itemCount == 0 )
+ 	    	    	 newCity = nextItem;
+ 	    	     if( itemCount == 1 )
+ 	    	    	 cityX = nextItem;
+ 	    	     if( itemCount == 2 )
+ 	    	    	 cityY = nextItem;
+ 	    	     itemCount++;
+			}
+			Point cityXY = new Point(Integer.parseInt(cityX),
+					Integer.parseInt(cityY));
+			coordHash.put(newCity, cityXY);
+    	}
+		
+		
+//		read edges into vertices
     	for ( int i =0; i < edges.size(); i++ ){
     		String newLine = edges.get(i);
 			StringTokenizer st = new StringTokenizer(newLine);
@@ -35,6 +74,7 @@ public class dijkstrasMap{
 			int itemCount = 0;
 			Vertex a;
 			Vertex b;
+//			Point cityCoord = new Point();
 			
 //			turn line from edge list into two cities and a distance
 			while (st.hasMoreTokens()) {		
@@ -50,19 +90,26 @@ public class dijkstrasMap{
 			}
 
 //			if city already in list, add edge to adjacency, 
-//			  otherwise make vertex
+//			  otherwise make vertex & add coordinates
 			if( !this.theCities.isEmpty() 
 					&& this.theCities.containsKey(cityA) ){
 				a = this.theCities.remove(cityA);
 				a.addAdjacent(cityB, dist);
+
 			}
 			else {
 				a = new Vertex(cityA);
 				a.addAdjacent(cityB, dist);
+				
+				Point cityCoord = new Point();
+				cityCoord = coordHash.get(cityA);			
+				a.setCoord(cityCoord);	
+				System.out.println(a.name+" "+a.coord.toString());
+
 			}
 			
 //			if city already in list, add edge to adjacency, 
-//			  otherwise make vertex (pairwise)
+//			  otherwise make vertex & add coordinates (pairwise)
 			if( !this.theCities.isEmpty() 
 					&& this.theCities.containsKey(cityB) ){
 				b = this.theCities.remove(cityB);
@@ -71,13 +118,17 @@ public class dijkstrasMap{
 			else {
 				b = new Vertex(cityB);
 				b.addAdjacent(cityA, dist);
-			}
 				
+				Point cityCoord = new Point();
+				cityCoord = coordHash.get(cityB);
+				b.setCoord(cityCoord);	
+				System.out.println(b.name+" "+b.coord.toString());
+			}
 			this.theCities.put(a.name, a);
 			this.theCities.put(b.name, b);	
 			}  	  
     	numNodes = this.theCities.size();
-    	}
+    }
     	
 	
       public void findPath( String s , String t ){    
@@ -113,6 +164,10 @@ public class dijkstrasMap{
 	    	
 	//    	if the polled vertex is not already known, check its adjacencies
 			if (!theCities.containsKey(knownCity.name)){
+//				if (knownCity.coord != null)
+//				    System.out.println(knownCity.name+" "+ knownCity.coord.toString());
+//				else 
+//					System.out.println("Coord not found");
 				
 	        	for( int i = 0; i < knownCity.adj.size(); i++){
 	        		if (i%2 == 0 && 
@@ -124,6 +179,7 @@ public class dijkstrasMap{
 			            Vertex existing = cityHash.get(knownCity.adj.get(i));
 			            newAdjacent.adj = existing.adj;
 			            newAdjacent.name = knownCity.adj.get(i);
+			            newAdjacent.coord = existing.coord;
 	
 			            newAdjacent.setDist(knownCity.dist+ 
 			            		Double.parseDouble(knownCity.adj.get(j)));
@@ -141,37 +197,63 @@ public class dijkstrasMap{
 			}
     	}	  	
     	pathLength = theCities.get(t).dist;
-    	pathNames = printPath(t);
+    	pathNames = stringPath(t);
     	
     	DecimalFormat df = new DecimalFormat("0.0");
     	StringPath = pathNames + ": "+df.format(pathLength)+" miles";
     	System.out.println(StringPath);
+    	
+    	pathPoints = coordPath(t);
+    	System.out.println(pathPoints.toString());
     }
       
 
       /*
        * public method to print path string
        */
-      public String printPath( String t ){
+      public String stringPath( String t ){
     	  StringBuilder stringPath = new StringBuilder();
-    	  StringBuilder stringPathFull = printPath( t, stringPath );
+    	  StringBuilder stringPathFull = stringPath( t, stringPath );
     	  return stringPathFull.toString();
       }
       
       /*
        * private method to print path string
        */
-      private StringBuilder printPath( String t, StringBuilder p ){
+      private StringBuilder stringPath( String t, StringBuilder p ){
     	  Vertex city = theCities.get(t); 
           if( city.path == null ){
-             p.append( city.name );
+             p.append( city.name + " " + city.coord.toString());
           }
           else{
-          printPath( city.path.name, p );
+          stringPath( city.path.name, p );
           p.append( " to " );
-          p.append( city.name );
+          p.append( city.name + " " + city.coord.toString());
           }
           return p;
       }
-    	
+    
+      /*
+       * public method to retrieve path points
+       */
+      public LinkedList<Point> coordPath( String t ){
+    	  LinkedList<Point> cPath = new LinkedList<>();
+    	  LinkedList<Point> cPathFull = coordPath( t, cPath );
+    	  return cPathFull;
+      }
+      
+      /*
+       * private method to retrieve path points
+       */
+      private LinkedList<Point> coordPath( String t, LinkedList<Point> c ){
+    	  Vertex city = theCities.get(t); 
+          if( city.path == null ){
+             c.add( city.coord );
+          }
+          else{
+          coordPath( city.path.name, c );
+          c.add( city.coord );
+          }
+          return c;
+      }
 }
